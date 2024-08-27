a74e2b0bba89ddf45a72392a73defbc9758dd183
Roberto Nares <roberto.nares@gmail.com>
2024-08-27 14:47:02 -0700

    Update game class state to include board instance

--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,22 @@
 # frozen_string_literal: true
 
 require_relative 'game_io'
+require_relative 'board'
 
 # Class for a chess game
 class Game
   include GameIO
 
+  def initialize(board = Board.new)
+    @board = board
+  end
+
   def play
     welcome
+    board.display
   end
+
+  private
+
+  attr_reader :board
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,11 +3,29 @@
 require_relative '../lib/game'
 
 describe Game do
-  subject(:game) { described_class.new }
+  let(:board) { double('board') }
+  subject(:game) { described_class.new(board) }
+
+  describe '#initialize' do
+    context 'when board instance is not passed as arguement' do
+      subject(:new_game) { described_class.new }
+
+      it 'creates a new board instance' do
+        expect(new_game.instance_variable_get(:@board)).not_to eq(board)
+      end
+    end
+
+    context 'when board instance is passed as argument' do
+      it 'sets it as the board instance variable' do
+        expect(game.instance_variable_get(:@board)).to eq(board)
+      end
+    end
+  end
 
   describe '#play' do
     before do
       allow(game).to receive(:welcome)
+      allow(board).to receive(:display)
     end
 
     it 'calls #welcome once' do
